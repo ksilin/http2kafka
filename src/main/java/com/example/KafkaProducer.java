b987f7aecc5d0790d6d97b29f545bcb4b9258ab5
ksilin <konstantin.siln@gmail.com>
2023-12-05 14:56:48 +0100

    consistent configs

--- src/main/java/com/example/KafkaProducer.java
+++ src/main/java/com/example/KafkaProducer.java
@@ -1,12 +1,9 @@
 package com.example;
 
-import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.reactive.messaging.*;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
-import java.util.stream.Stream;
 
 @ApplicationScoped
 public class KafkaProducer {
@@ -19,13 +16,5 @@ public class KafkaProducer {
         // Process the payload
         System.out.println("Received payload: " + payload);
         emitter.send(payload);
-    }
-
-    /**
-     * Sends message to the "words-out" channel, can be used from a JAX-RS resource or any bean of your application.
-     * Messages are sent to the broker.
-     **/
-    void onStart(@Observes StartupEvent ev) {
-        Stream.of("Hello", "with", "SmallRye", "reactive", "message").forEach(string -> emitter.send(string));
     }
 }
