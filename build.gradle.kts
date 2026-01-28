e492e73986fdea47fef4842572a4ad80f8723bf7
ksilin <konstantin.silin@gmail.com>
2026-01-28 08:54:56 +0100

    update to quarkus 3.30.8 & jackson 2.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,9 @@ version = "1.0.0-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks.withType<Test> {
